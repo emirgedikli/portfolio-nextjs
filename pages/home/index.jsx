15094bafbc7ee26549f4b7ef92a8262c2831de93
emirgedikli <emirwtaha@gmail.com>
2022-10-11 15:06:23 +0200

    all

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -1,37 +1,11 @@
-import GitHubIcon from '@mui/icons-material/GitHub';
-import Image from 'next/image'
-import dream from '../../src/assets/img/dream.jpg'
+import HomePage from '@pages/home/content'
 
-let styles = {
-    flex: "flex",
-    flexCol: "flex-col",
-    itemsCenter: "items-center",
-    justifyCenter: "justify-center",
-    textBlack: "text-black",
-    wScreen: "w-screen",
-    hScreen: "h-screen",
-    fontBold: "font-bold",
-    text4xl: "text-4xl",
-    py8: "py-8",
-    textXl: "text-xl"
-};
-
-const HomePage = () => {
+const Page = () => {
     return (
-        <div className={`${styles.flex} ${styles.flexCol} ${styles.itemsCenter} ${styles.justifyCenter} ${styles.textBlack} ${styles.wScreen} ${styles.hScreen}`}>
-            <div>
-                <Image src={dream} alt="" width="350px" height="350px" />
-            </div>
-            <div>
-                <h1 className={`${styles.fontBold} ${styles.text4xl} ${styles.py8}`}>Emir Gedikli</h1>
-                <p className={`${styles.textXl}`}>Développeur Web Front-End</p>
-            </div>
-            <div className={`${styles.flex} ${styles.flexCol}`}>
-                    <GitHubIcon />
-                    <h3>Github</h3>
-            </div>
-        </div>
+        <>
+            <HomePage />
+        </>
     )
 }
 
-export default HomePage;
+export default Page
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 
+const { colors } = require("@mui/material");
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        darkBlue: "#1f2937",
+      },
+      ...colors,
+    },
   },
   plugins: [],
-}
+};
