33e1df67c0476ed895f6c8ca3c691da23d9f7b37
emirgedikli <emirwtaha@gmail.com>
2022-10-12 19:16:36 +0200

    12-10-22

--- pages/_document.js
+++ pages/_document.js
@@ -4,10 +4,16 @@ export default function Document() {
     return (
         <Html lang="fr">
             <Head>
+            <link rel="shortcut icon" href="../src/assets/img/myAvatar.png" />
+            {/* photo title */}
             <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
             <link href="https://fonts.googleapis.com/css2?family=Cinzel&display=swap" rel="stylesheet" />
             {/* fontCinzel */}
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+            <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300&display=swap" rel="stylesheet" />
+            {/* fontQuicksand */}
             </Head>
             <body className="bg-[#1f2937]">
                 <Main />
--- src/layout/header.jsx
+++ src/layout/header.jsx
@@ -3,7 +3,10 @@ import profil from '@src/assets/img/profil.png'
 
 let styles = {
     IDdiv: "flex flex-col items-center text-black w-screen h-screen",
-    IDdiv1h1: "text-4xl py-8 text-xl fontCinzel text-white opacity-70 text-3xl",
+    IDdiv1h1: "text-4xl py-8 fontCinzel text-white opacity-60 text-4xl",
+    IDdiv2: "flex justify-around text-lg fontQuicksand text-white opacity-90 pb-8",
+    IDdiv2cv: "px-4",
+    IDdiv2contact: "font-black bg-blue-500 rounded-md px-2 opacity-100",
     IDdiv3: "flex flex-col",
     imgProfil: "rounded-3xl"
 };
@@ -17,12 +20,14 @@ const HomePage = () => {
                 <h1>Emir Gedikli</h1>
             </div>
             {/* ID div2 */}
-            <div>
-                <Image src={profil} alt="profil" height="300px" width="300px" className={styles.imgProfil} />
+            <div className={styles.IDdiv2}>
+                    <button>Accueil</button>
+                    <button className={styles.IDdiv2cv}>cv</button>
+                    <button className={styles.IDdiv2contact}>Contact</button>
             </div>
             {/* ID div3 */}
             <div className={styles.IDdiv3}>
-
+                <Image src={profil} alt="profil" height="300px" width="300px" className={styles.imgProfil} />
             </div>
         </div>
     )
