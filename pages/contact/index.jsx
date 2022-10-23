07208a5b69db075e8221aaf6e790b1fdac891275
emirgedikli <emirwtaha@gmail.com>
2022-10-23 13:42:55 +0200

    update

--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -4,7 +4,7 @@ import Links from "@src/components/links/index";
 
 let styles = {
   header: "bg-[#1f2937] pt-10 pb-4 px-10 lg:pb-16",
-  body: "bg-[#1f2937] px-10 py-32 fontNunito text-white opacity-95 lg:px-10",
+  body: "bg-[#1f2937] px-10 py-32 fontNunito text-white opacity-95 lg:px-10 lg:py-32",
 };
 
 const Contact = () => {
