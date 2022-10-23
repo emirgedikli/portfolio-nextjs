3411dcb7d4bce394fc47e2e1f60d5abdcfb41284
emirgedikli <emirwtaha@gmail.com>
2022-10-23 13:39:41 +0200

    update

--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -1,11 +1,11 @@
 import Header from "@src/components/header/index";
 import Newsletter from "@src/components/newsletter";
-import Footer from "@src/components/footer/index";
+import Links from "@src/components/links/index";
 
 let styles = {
   header: "bg-[#1f2937] pt-10 pb-4 px-10 lg:pb-16",
-  body: "bg-[#1f2937] px-10 py-24 fontNunito text-white opacity-95 lg:px-10",
-  footer: "bg-[#1f2937]"
+  body: "bg-[#1f2937] px-10 pt-36 fontNunito text-white opacity-95 lg:px-10",
+  Links: "pt-44 pb-20",
 };
 
 const Contact = () => {
@@ -16,9 +16,9 @@ const Contact = () => {
       </div>
       <div className={styles.body}>
         <Newsletter />
-      </div>
-      <div className={styles.footer}>
-        <Footer />
+        <div className={styles.Links}>
+          <Links />
+        </div>
       </div>
     </>
   );
--- src/components/newsletter/index.jsx
+++ src/components/newsletter/index.jsx
@@ -1,53 +1,119 @@
 import Image from "next/image";
 import Newsletterimg from "@src/assets/img/newsletterComp/newsletter.png";
 
-let styles = {
-  NSdiv:
-    "container md:w-2/3 xl:w-auto mx-auto flex flex-col xl:items-stretch justify-between xl:flex-row",
-  NSdiv1:
-    "xl:w-1/2 md:mb-14 xl:mb-0 relative h-auto flex items-center justify-center",
-  NSdiv1img: "h-full xl:w-full lg:w-1/2 w-full",
-  NSdiv2: "w-full xl:w-1/2 xl:pl-40 xl:py-28",
-  NSdiv2h1:
-    "text-white opacity-70 text-2xl md:text-4xl xl:text-5xl font-bold leading-10 mb-4 text-center xl:text-left md:mt-0 mt-4",
-  NSdiv2p:
-    "text-base leading-normal text-white opacity-20 text-center xl:text-left lg:pr-32",
-  NSdiv3: "flex items-stretch mt-12",
-  NSdiv3input:
-    "bg-gray-100 rounded-lg rounded-r-none text-base leading-none text-gray-800 p-5 w-4/5 border border-transparent focus:outline-none focus:border-gray-500",
-  NSdiv3btn:
-    "w-32 rounded-l-none hover:bg-indigo-600 bg-indigo-700 rounded text-base font-medium leading-none text-white p-5 uppercase focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700",
-};
+let styles = {};
 
 const Newsletter = () => {
   return (
     <>
-      <div className={styles.NSdiv}>
-        <div className={styles.NSdiv1}>
-          <Image
-            src={Newsletterimg}
-            alt="newsletterimg"
-            className={styles.NSdiv1img}
-          />
+      <section class="w-full max-w-2xl px-6 py-4 mx-auto bg-white rounded-md shadow-md dark:bg-gray-800">
+        <h2 class="text-3xl font-semibold text-center text-gray-800 dark:text-white">
+          Contactez-moi
+        </h2>
+
+        <p class="mt-3 text-center text-gray-600 dark:text-gray-400">
+          Vous avez des idées à partager avec moi ? Écrivez maintenant !
+        </p>
+
+        <div class="grid grid-cols-1 gap-6 mt-6 sm:grid-cols-2 md:grid-cols-3">
+          <a
+            href="#"
+            class="flex flex-col items-center px-4 py-3 text-gray-700 transition-colors duration-300 transform rounded-md dark:text-gray-200 hover:bg-blue-200 dark:hover:bg-blue-500"
+          >
+            <svg
+              class="w-5 h-5"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
+                clip-rule="evenodd"
+              />
+            </svg>
+
+            <span class="mt-2">Paris 75000</span>
+          </a>
+
+          <a
+            href="#"
+            class="flex flex-col items-center px-4 py-3 text-gray-700 transition-colors duration-300 transform rounded-md dark:text-gray-200 hover:bg-blue-200 dark:hover:bg-blue-500"
+          >
+            <svg
+              class="w-5 h-5"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
+            </svg>
+
+            <span class="mt-2">+33767245024</span>
+          </a>
+
+          <a
+            href="#"
+            class="flex flex-col items-center px-4 py-3 text-gray-700 transition-colors duration-300 transform rounded-md dark:text-gray-200 hover:bg-blue-200 dark:hover:bg-blue-500"
+          >
+            <svg
+              class="w-5 h-5"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
+              <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
+            </svg>
+
+            <span class="mt-2">emirwtaha@gmail.com</span>
+          </a>
         </div>
-        <div className={styles.NSdiv2}>
-          <h1 className={styles.NSdiv2h1}>Écrivez-moi</h1>
-          <p className={styles.NSdiv2p}>
-            Pour me faire part de vos idées, vous pouvez m&apos;envoyer un message en
-            utilisant la boîte de messages ci-dessous. Mes coordonnées se trouvent en bas de la page.
-          </p>
-          <div className={styles.NSdiv3}>
-            <input
-              className={styles.NSdiv3input}
-              type="email"
-              placeholder="Rédiger un message"
-            />
-            <button className={styles.NSdiv3btn}>envoyer</button>
+
+        <div class="mt-6 ">
+          <div class="items-center -mx-2 md:flex">
+            <div class="w-full mx-2">
+              <label class="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200">
+                Nom & Prénom
+              </label>
+
+              <input
+                class="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                type="text"
+              />
+            </div>
+
+            <div class="w-full mx-2 mt-4 md:mt-0">
+              <label class="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200">
+                E-mail
+              </label>
+
+              <input
+                class="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                type="email"
+              />
+            </div>
+          </div>
+
+          <div class="w-full mt-4">
+            <label class="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200">
+              Message
+            </label>
+
+            <textarea class="block w-full h-40 px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"></textarea>
+          </div>
+
+          <div class="flex justify-center mt-6">
+            <button class="px-4 py-2 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">
+              Envoyer
+            </button>
           </div>
         </div>
-      </div>
+      </section>
     </>
   );
 };
 
 export default Newsletter;
+
+// https://merakiui.com/components/contact
