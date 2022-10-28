0f51f4af596966b8ac3b3f1fbd24ab5553ebd454
emirgedikli <emirwtaha@gmail.com>
2022-10-28 16:24:53 +0200

    bg-transparent

--- src/components/newsletter/index.jsx
+++ src/components/newsletter/index.jsx
@@ -19,11 +19,11 @@ let styles = {
   div2divLabel: "w-full mx-2 mt-4 md:mt-0",
   email: "block mb-2 text-sm font-medium text-white",
   inputEmail:
-    "block w-full px-4 py-2 text-white border rounded-md dark:bg-gray-800 dark:border-gray-700 focus:border-blue-700 focus:ring-blue-700 dark:focus:border-blue-800 focus:outline-none focus:ring focus:ring-opacity-40",
+    "block w-full px-4 py-2 text-white bg-transparent border rounded-md dark:bg-gray-800 dark:border-gray-700 focus:border-blue-700 focus:ring-blue-700 dark:focus:border-blue-800 focus:outline-none focus:ring focus:ring-opacity-40",
   div2divB: "w-full mt-4",
   message: "block mb-2 text-sm font-medium text-white",
   divBtextarea:
-    "block w-full h-40 px-4 py-2 text-white border rounded-md dark:bg-gray-800 dark:border-gray-700 focus:border-blue-700 dark:focus:border-blue-800 focus:outline-none focus:ring focus:ring-blue-800 focus:ring-opacity-40",
+    "block w-full h-40 px-4 py-2 text-white bg-transparent border rounded-md dark:bg-gray-800 dark:border-gray-700 focus:border-blue-700 dark:focus:border-blue-800 focus:outline-none focus:ring focus:ring-blue-800 focus:ring-opacity-40",
   div2divC: "flex justify-center mt-6",
   sendBtn:
     "px-4 py-2 text-white transition-colors duration-300 transform bg-gray-800 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600",
