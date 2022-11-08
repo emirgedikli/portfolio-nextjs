dbc559d3c1d5d7029517743be46815a84a0db56e
emirgedikli <emirwtaha@gmail.com>
2022-11-08 13:59:39 +0100

    profil

--- src/components/introduction/index.jsx
+++ src/components/introduction/index.jsx
@@ -41,7 +41,7 @@ const Introduction = () => {
             </h2>
           </div>
           <div className={styles.div3}>
-            <h1>Développeur Web Front-End</h1>
+            <h1>Développeur Web</h1>
           </div>
           <div className={styles.div4}>
             <p>
