8f6eb501a41f7d41fd7c0adfae10fb27e8a62b19
emirgedikli <emirwtaha@gmail.com>
2022-11-17 10:06:06 +0100

    styles

---
+++ pages/404/styles.jsx
@@ -0,0 +1,13 @@
+let styles = {
+  main: "h-screen w-full flex flex-col justify-center items-center bg-[#1f2937]",
+  mainh1: "text-9xl font-extrabold text-white tracking-widest",
+  maindiv: "bg-[#FF6A3D] px-2 text-sm rounded rotate-12 absolute",
+  mainSpan1: "mt-5",
+  mainBtn:
+    "relative inline-block text-sm font-medium text-[#FF6A3D] group active:text-orange-500 focus:outline-none focus:ring",
+  mainSpan2:
+    "absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-[#FF6A3D] group-hover:translate-y-0 group-hover:translate-x-0",
+  mainSpan3: "relative block px-8 py-3 bg-[#1f2936] border border-current",
+};
+
+export default styles;
--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -1,3 +1,4 @@
+import styles from "@pages/home/styles";
 import Head from "next/head";
 import Header from "@src/components/header/index";
 import Introduction from "@src/components/introduction/index";
@@ -5,19 +6,6 @@ import ViewProjects from "@src/components/viewprojects";
 import IworkWith from "@src/components/iworkwith";
 import Newsletter from "@src/components/newsletter";
 import Footer from "@src/components/footer/index";
-
-let styles = {
-  header: "bg-[#1f2937]",
-  introduction:
-    "bg-[#1f2937] px-10 py-16 fontNunito text-white opacity-95 lg:pr-32 lg:pt-52 lg:pb-52",
-  viewprojects:
-    "bg-[#1f2937] px-10 py-16 fontNunito text-white opacity-95 lg:pr-32 lg:pt-52 lg:pb-52",
-  iworkwith:
-    "bg-[#1f2937] px-10 py-16 fontNunito text-white opacity-95 lg:flex lg:justify-center lg:py-52",
-  newsletter:
-    "bg-[#1f2937] px-10 pb-28 pt-16 fontNunito text-white opacity-95 lg:pr-32 lg:pb-52 -lg:pt-10",
-  footer: "bg-[#1f2937]",
-};
 
 const HomePage = () => {
   return (
