162af1c893a5b86434ad1097217c50e5ffe5c3d8
emirgedikli <emirwtaha@gmail.com>
2022-11-20 19:54:59 +0100

    retouche layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ const { colors } = require("@mui/material");
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       ...colors,
