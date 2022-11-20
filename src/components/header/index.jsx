9cecc817cb8dbe8fbb8516438aae20efedd555fb
emirgedikli <emirwtaha@gmail.com>
2022-11-20 12:43:10 +0100

    update

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -4,13 +4,16 @@ import "aos/dist/aos.css";
 let styles = {
   header: "lg:flex lg:py-10 lg:w-full",
   name: "flex justify-center w-full py-8 lg:flex lg:justify-center lg:w-full",
+  cursor: "cursor-pointer",
 };
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.name}>
-        <Name />
+        <div className={styles.cursor}>
+          <Name />
+        </div>
       </div>
     </header>
   );
--- src/components/newsletter/index.jsx
+++ src/components/newsletter/index.jsx
@@ -24,7 +24,7 @@ let styles = {
     "block w-full h-40 px-4 py-2 text-white bg-transparent border rounded-md dark:bg-gray-800 dark:border-white focus:border-white focus:ring-white dark:focus:border-white-800 focus:outline-none focus:ring focus:ring-opacity-40",
   div2divC: "flex justify-center mt-6",
   sendBtn:
-    "px-4 py-2 text-white transition-colors duration-300 transform bg-transparent border rounded-md dark:bg-gray-800 dark:border-white focus:border-white focus:ring-white dark:focus:border-white-800 focus:outline-none focus:ring focus:ring-opacity-40",
+    "px-4 py-2 text-white cursor-pointer transition-colors duration-300 transform bg-transparent border rounded-md dark:bg-gray-800 dark:border-white focus:border-white focus:ring-white dark:focus:border-white-800 focus:outline-none focus:ring focus:ring-opacity-40",
 };
 
 const Newsletter = () => {
@@ -89,14 +89,14 @@ const Newsletter = () => {
             </div>
 
             <div className={styles.div2divC}>
-              <button
+              <a
+                href="/"
                 data-aos="zoom-in-up"
                 data-aos-once="false"
                 className={styles.sendBtn}
-                type="submit"
               >
                 Envoyer
-              </button>
+              </a>
             </div>
           </form>
         </div>
